50d771a0b21877afb4ebbb1e6fe06b21c88e89f3
Tom <tom.westwood2000@gmail.com>
2021-11-07 23:21:43 +0000

    Added support for stadium names

--- app.py
+++ app.py
@@ -15,9 +15,13 @@ if __name__ == "__main__":
     # "CLUB_LONG_NAME_CHANGE" 680 "Manchester United" ""
 
     # Menu selection
-    # print("Choose your option: \n" + "1. Club Long Name\n" + "2. Club Short Name\n")
-    print("Choose your option: \n" + "1. Club Long Name\n" )
+    print("Choose your option: \n" + "1. Club Long Name\n" + "2. Club Short Name\n" + "3. Stadium Name")
     selection = input()
+
+    # 1. Club Long Name
+    # 2. Club Short Name
+    # 3. Stadium Name Change
+    # 4. City Name Change
 
     # if statement - choose long name
     if selection == "1":
@@ -27,4 +31,24 @@ if __name__ == "__main__":
             lang = stringify(" ")
 
             newfile.write(clnc + " " + tid + " " + tmnm + " " + lang + '\n')
+            newfile.close()
+
+            # if statement - choose short name
+    if selection == "2":
+            csnc = stringify("CLUB_SHORT_NAME_CHANGE")
+            tid = input("Team ID: ")
+            tmnm = stringify(input("Team Name: "))
+            lang = stringify(" ")
+
+            newfile.write(csnc + " " + tid + " " + tmnm + " " + lang + '\n')
+            newfile.close()
+
+            # if statement - choose stadium name
+    if selection == "3":
+            stnc = stringify("STADIUM_NAME_CHANGE")
+            sid = input("Stadium ID: ")
+            stnm = stringify(input("Stadium Name Name: "))
+            lang = stringify(" ")
+
+            newfile.write(stnc + " " + sid + " " + stnm + " " + lang + '\n')
             newfile.close()
