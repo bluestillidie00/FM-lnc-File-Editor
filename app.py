ff219e158c178edad276829956290cfde303b101
Tom <tom.westwood2000@gmail.com>
2021-11-07 23:46:43 +0000

    Added support for fake national team, player pics and player name changes

--- app.py
+++ app.py
@@ -12,16 +12,24 @@ if __name__ == "__main__":
     # newfile.write("# Created by " + header + "\n")
     # newfile.close()
 
-    # "CLUB_LONG_NAME_CHANGE" 680 "Manchester United" ""
-
-    # Menu selection
-    print("Choose your option: \n" + "1. Club Long Name\n" + "2. Club Short Name\n" + "3. Stadium Name\n"+ "4. City Name Change\n")
-    selection = input()
-
     # 1. Club Long Name
     # 2. Club Short Name
     # 3. Stadium Name Change
     # 4. City Name Change
+    # 5. Fake National Team
+    # 6. Disable Player Pics
+    # 7. Person name change
+
+    # Menu selection
+    print("Choose your option: \n" + "1. Club Long Name\n" +
+          "2. Club Short Name\n" +
+          "3. Stadium Name\n" +
+          "4. City Name Change\n" +
+          "5. Fake National Team\n" +
+          "6. Disable Player Pics\n" +
+          "7. Person Name Change\n")
+
+    selection = input()
 
     # if statement - choose long name
     if selection == "1":
@@ -61,4 +69,33 @@ if __name__ == "__main__":
             lang = stringify(" ")
 
             newfile.write(ctnc + " " + cid + " " + ctnm + " " + lang + '\n')
+            newfile.close()
+
+            # if statement - Fake National Team
+    if selection == "5":
+            fknt = stringify("FAKE_NATIONAL_TEAM")
+            nid = input("Nation ID: ")
+            onoff = input("On/Off (Type 1 for On, 0 for off): ")
+
+            newfile.write(fknt + " " + nid + " " + onoff + "  " + '\n')
+            newfile.close()
+
+            # if statement - Disable Player Pics
+    if selection == "6":
+            dspp = stringify("DISABLE_PLAYER_PICS")
+            pid = input("Person ID: ")
+            onoff = input("On/Off (Type 1 for On, 0 for off): ")
+
+            newfile.write(dspp + " " + pid + " " + onoff + "  " + '\n')
+            newfile.close()
+
+            # if statement - Person Name Changes
+    if selection == "7":
+            psnm = stringify("CHANGE_PLAYER_NAME")
+            pid = input("Person ID: ")
+            finm = stringify(input("First Name: "))
+            cmnm = stringify(input("Common Name: "))
+            sunm = stringify(input("Surname: "))
+
+            newfile.write(psnm + " " + pid + " " + finm + " " + cmnm + " " + sunm + '\n')
             newfile.close()
